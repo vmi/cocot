cb6769a8b8f62414e6b2fafbb7846cc44c0ed17a
Motonori IWAMURO <vmi@nifty.com>
2012-03-13 13:43:59 +0900

    a bit fix.

--- mktag.sh
+++ mktag.sh
@@ -10,9 +10,10 @@ fi
 
 ver=`perl -nle '/^AC_INIT\([^,]+,\[([^\]]+)\]\)/ && print $1' configure.ac`
 
-cmd="git tag cocot-$ver"
+cmd="git tag -f cocot-$ver"
 
 if $run; then
+  echo "[$cmd -m '$1']"
   eval "$cmd -m '$1'"
 else
   echo "[$cmd]"
